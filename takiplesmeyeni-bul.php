4af4e8bb6e7d4aac45fd55458233418f6e900bb7
Tayfun Güler <gulertayfun@outlook.com>
2022-01-12 14:34:01 +0300

    Create takiplesmeyeni-bul.php

---
+++ takiplesmeyeni-bul.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Takip Edilenler */
+$takip_edilen = "
+dyfunn
+tayfungulerorg
+instagram
+zuck
+elonrmuskk
+";
+
+/* Takipçiler  */
+$takipci = "
+dyfunn
+elonrmuskk
+";
+
+/* String to Array */
+$takip_edilen = preg_replace('/\s+/',',',str_replace(array("\r\n","\r","\n"),' ',trim($takip_edilen)));
+$takip_edilen_ar = explode(",", $takip_edilen);
+
+$takipci = preg_replace('/\s+/',',',str_replace(array("\r\n","\r","\n"),' ',trim($takipci)));
+$takipci_ar = explode(",", $takipci);
+
+
+/* Compare Two Array */
+$takiplesmeyenler = array_diff($takip_edilen_ar, $takipci_ar);
+
+print_r($takiplesmeyenler);
